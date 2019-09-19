e5aa61ff6a85e5266cf5720961c7e43b40bdae22
Alberto Jarabo <alberto.jarabo@atmira.com>
2019-09-19 12:52:06 +0200

    example1

---
+++ src/main/java/com/ajarabo/ci_example/App.java
@@ -0,0 +1,13 @@
+package com.ajarabo.ci_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Demo app for continuos integration" );
+    }
+}
